84591554ef30a478fb36c7648613ee991d1daeed
Dung1109 <124704925+Dung1109@users.noreply.github.com>
2024-12-04 02:29:04 +0700

    add data

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import LoginPage from "@/components/login-01";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 export default function Page() {
     const [data, setData] = useState("");
+    const [data2, setData2] = useState("");
+    const [data3, setData3] = useState("");
+    const [token, setToken] = useState("");
 
-    useEffect(() => {
-        fetch("http://127.0.0.1:8080/secret", {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    useEffect( () => {
+         fetch("http://127.0.0.1:8080/secret", {
             method: "GET",
             credentials: "include",
             headers: {
@@ -22,12 +27,65 @@ export default function Page() {
                 setData(data);
             })
             .catch((err) => console.log(err));
+
+         fetch("http://127.0.0.1:8080/admin", {
+            method: "GET",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                Origin: "http://127.0.0.1:3000",
+            },
+        })
+            .then((res) => res.text())
+            .then((data) => {
+                console.log("Response data:");
+
+                setData2(data);
+            })
+            .catch((err) => console.log(err));
+
+         fetch("http://127.0.0.1:8080/user", {
+            method: "GET",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                Origin: "http://127.0.0.1:3000",
+            },
+        })
+            .then((res) => res.text())
+            .then((data) => {
+                console.log("Response user data:");
+
+                setData3(data);
+            })
+            .catch((err) => console.log(err));
+
+        fetch("http://127.0.0.1:8080/access_token", {
+            method: "GET",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                Origin: "http://127.0.0.1:3000",
+            },
+        })
+            .then((res) => res.text())
+            .then((data) => {
+                console.log("Response user data:");
+
+                setToken(data);
+            })
+            .catch((err) => console.log(err));
     }, []);
 
     return (
-        <div className="flex h-screen w-full items-center justify-center px-4">
+        <div /*className="flex h-screen w-full items-center justify-center px-4"*/>
             {data}
-            <LoginPage />
+            <br/>
+            {data2}
+            {/*<br/>*/}
+            {/*{`User: ${data3}`}*/}
+            {/*<br/>*/}
+            {/*{`Token: ${token}`}*/}
         </div>
     );
 }
